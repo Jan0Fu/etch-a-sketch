c7e5e3ff56e57bd9dffbaf8b138db136f612d63a
Jan0Fu <115988224+Jan0Fu@users.noreply.github.com>
2022-10-29 13:34:50 +0200

    Completed main features of etch a sketch project

--- etchasketch.js
+++ etchasketch.js
@@ -1,6 +1,56 @@
-const container = document.querySelector('.container');
+const gridsize = document.querySelector('#btn-grid');
+const gridclear= document.querySelector('#btn-clear');
+const gridcontainer = document.querySelector('.grid-container')
+let a, b, c = 0
 
-for (let i = 1; i < 257; i++) {
-  const div = document.createElement('div');
-  container.appendChild(div);
+window.addEventListener('load', gridDefault);
+
+function gridDefault() {
+    for (let i=0; i<20 * 20; i++) {
+        const divcreate = document.createElement('div');
+        gridcontainer.appendChild(divcreate);
+    }
+    paint();  
+}
+
+function paint() {
+    document.querySelectorAll('.grid-container div').forEach(div => {
+        div.addEventListener('mouseover', (e) => {
+            randColor()
+            e.target.style.backgroundColor = 'rgb(' + a + ',' + b + ',' + c + ')';
+        })
+    }) 
+}
+
+gridsize.addEventListener('click', () => {
+    gridinput = prompt("Enter number between 1 to 60.")
+    if (0 < gridinput && gridinput < 61) {
+        gridcontainer.setAttribute('style', `grid-template-columns: repeat(${gridinput}, 1fr); grid-template-rows: repeat(${gridinput}, 1fr);`)
+        while (gridcontainer.firstChild) {
+            gridcontainer.firstChild.remove()
+        }
+        for (let i=0; i<gridinput**2; i++) {
+            const divcreate = document.createElement('div');
+            gridcontainer.appendChild(divcreate);
+        }
+        document.querySelectorAll('.grid-container div').forEach(div => {
+            div.style.backgroundColor = "white";
+        })
+        paint();
+    }
+    else {
+        alert("Please enter whole number between 1 to 60.")
+    }
+})
+
+gridclear.addEventListener('click', () => {
+    document.querySelectorAll('.grid-container div').forEach(div => {
+        div.style.backgroundColor = "white";
+    })
+})
+
+function randColor() {
+    a = Math.floor(Math.random() * 256);
+    b = Math.floor(Math.random() * 256);
+    c = Math.floor(Math.random() * 256);
 }
